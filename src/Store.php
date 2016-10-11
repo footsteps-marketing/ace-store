eef2781b79941bbef24876e396c4716f5b5fa66c
Aaron Hipple <aaron@footstepsmarketing.com>
2016-10-11 17:12:44 -0600

    Move caching into magic method, clean up initialized properties

--- src/Store.php
+++ src/Store.php
@@ -9,148 +9,34 @@ use Exception;
  */
 class Store
 {
-    /**
-     * URL format for requesting JSON data from Ace's store locator
-     */
     const ACE_REQUEST_URL_FORMAT = "http://www.acehardware.com/storeLocServ?heavy=true&token=ACE&operation=storeData&storeID=%05d";
-
-    /**
-     * URL format for requesting JSON data from Ace's store locator
-     */
     const ACE_LANDING_PAGE_URL_FORMAT = "http://www.acehardware.com/mystore/index.jsp?store=%05d";
 
-    /**
-     * The path to the local cache folder
-     * @var string
-     */
-    private $cacheFolder = null;
+    private $locationName;
+    private $address;
+    private $postalCode;
+    private $stateCode;
+    private $city;
+    private $phoneNumber;
+    private $storeInfoURL;
+    private $storeBiography;
+    private $owner;
+    private $staff;
+    private $storeNumber;
+    private $mapCoords;
+    private $hours;
+    private $departments;
+    private $services;
+    private $brands;
+    private $chain;
 
-    /**
-     * Time (in seconds) to cache data for any particular store
-     * @var int
-     */
-    private $cacheLifetime = null;
-
-    /**
-     * The store number described by this object
-     * @var int
-     */
-    private $storeNumber = null;
-
-    /**
-     * The URL to request for this store's store info
-     * @var string
-     */
-    private $url = null;
-
-    /**
-     * The JSON response body regarding this store
-     * @var string
-     */
-    private $body = null;
-
-    /**
-     * The decoded store info object
-     * @var object
-     */
-    private $storeObject = null;
-
-    /**
-     * The store's name from Ace
-     * @var string
-     */
-    private $locationName = null;
-
-    /**
-     * The store's address
-     * @var string
-     */
-    private $address = null;
-
-    /**
-     * The store's postal code
-     * @var string
-     */
-    private $postalCode = null;
-
-    /**
-     * The store's state (postal code style)
-     * @var string
-     */
-    private $stateCode = null;
-
-    /**
-     * The store's city
-     * @var string
-     */
-    private $city = null;
-
-    /**
-     * The store's phone number
-     * @var string
-     */
-    private $phoneNumber = null;
-
-    /**
-     * The store's longitude
-     * @var float
-     */
-    private $longitude = null;
-
-    /**
-     * The store's latitude
-     * @var float
-     */
-    private $latitude = null;
-
-    /**
-     * The store's website (or their landing page on acehardware.com)
-     * @var string
-     */
-    private $storeInfoURL = null;
-
-    /**
-     * The store's hours (by day)
-     * @var array
-     */
-    private $hours = null;
-
-    /**
-     * The store's departments
-     * @var array
-     */
-    private $departments = null;
-
-    /**
-     * The store's services
-     * @var array
-     */
-    private $services = null;
-
-    /**
-     * The store's brands
-     * @var array
-     */
-    private $brands = null;
-
-    /**
-     * The store's owner(s)
-     * @var string
-     */
-    private $owner = null;
-
-    /**
-     * The store's staff
-     * @var array
-     */
-    private $staff = null;
-
-    /**
-     * The store's "biography"
-     * @var string
-     */
-    private $storeBiography = null;
-
+    private $cacheFolder;
+    private $cacheLifetime;
+    private $url;
+    private $body;
+    private $storeObject;
+    private $longitude;
+    private $latitude;
 
 
     /**
@@ -160,13 +46,13 @@ class Store
      * in the constant `FSM_ACE_CACHE_FOLDER` -- cache lifetime to be specified in the
      * constant `FSM_ACE_CACHE_LIFETIME` (as an integer, in seconds)
      *
-     * @param int    $storeNumber The store number
+     * @param int $storeNumber The store number
      * @param string $cacheFolder Path to the cache folder
      */
     public function __construct(int $storeNumber, $cacheFolder = null)
     {
         $this->cacheFolder = $cacheFolder;
-        
+
         if (is_null($this->cacheFolder) && defined('FSM_ACE_CACHE_FOLDER')) {
             $this->cacheFolder = FSM_ACE_CACHE_FOLDER;
         }
@@ -186,13 +72,13 @@ class Store
      */
     public function __get($property)
     {
-        if ($this->$property) {
+        if (property_exists($this, $property) && $this->$property) {
             return $this->$property;
         }
         $propertyGetter = sprintf('get%s', ucfirst($property));
-        return $this->$propertyGetter();
+        $this->$property = $this->$propertyGetter();
+        return $this->$property;
     }
-
 
 
     /**
@@ -203,7 +89,8 @@ class Store
         $cachePath = $this->cacheFolder . '/' . $this->storeNumber . '.json';
         if (is_null($this->cacheFolder) ||
             time() - filemtime($cachePath) > $this->cacheLifetime ||
-            !is_readable($cachePath)) {
+            !is_readable($cachePath)
+        ) {
             $client = new Client();
             $response = $client->get($this->url);
 
@@ -222,19 +109,14 @@ class Store
     }
 
 
-
     /**
      * Get the location name
      * @return string
      */
     public function getLocationName()
     {
-        if (is_null($this->locationName)) {
-            $this->locationName = $this->storeObject->locationName;
-        }
-        return $this->locationName;
+        return $this->storeObject->locationName;
     }
-
 
 
     /**
@@ -243,16 +125,14 @@ class Store
      */
     public function getAddress()
     {
-        if (is_null($this->address)) {
-            $i = 1;
-            while (property_exists($this->storeObject, "address{$i}")) {
-                $this->address[] = $this->storeObject->{"address{$i}"};
-                $i++;
-            }
+        $address = [];
+        $i = 1;
+        while (property_exists($this->storeObject, "address{$i}")) {
+            $address[] = $this->storeObject->{"address{$i}"};
+            $i++;
         }
-        return implode(',', $this->address);
+        return implode(',', $address);
     }
-
 
 
     /**
@@ -261,12 +141,8 @@ class Store
      */
     public function getPostalCode()
     {
-        if (is_null($this->postalCode)) {
-            $this->postalCode = $this->storeObject->postalCode;
-        }
-        return $this->postalCode;
+        return $this->storeObject->postalCode;
     }
-
 
 
     /**
@@ -275,12 +151,8 @@ class Store
      */
     public function getStateCode()
     {
-        if (is_null($this->stateCode)) {
-            $this->stateCode = $this->storeObject->stateCode;
-        }
-        return $this->stateCode;
+        return $this->storeObject->stateCode;
     }
-
 
 
     /**
@@ -289,12 +161,8 @@ class Store
      */
     public function getCity()
     {
-        if (is_null($this->city)) {
-            $this->city = $this->storeObject->city;
-        }
-        return $this->city;
+        return $this->storeObject->city;
     }
-
 
 
     /**
@@ -303,28 +171,8 @@ class Store
      */
     public function getPhoneNumber()
     {
-        if (is_null($this->phoneNumber)) {
-            $this->phoneNumber = $this->phoneNumberfy($this->storeObject->phoneNumber);
-        }
-        return $this->phoneNumber;
+        return $this->phoneNumberfy($this->storeObject->phoneNumber);
     }
-
-
-
-    /**
-     * Format a phone number in the US-style
-     *
-     * @param $number
-     * @return string
-     */
-    private function phoneNumberfy($number)
-    {
-        if (preg_match('/^(\d{3})(\d{3})(\d{4})$/', (string) $number, $matches)) {
-            return sprintf('(%s) %s-%s', $matches[1], $matches[2], $matches[3]);
-        }
-        return (string) $number;
-    }
-
 
 
     /**
@@ -333,12 +181,8 @@ class Store
      */
     public function getStoreInfoURL()
     {
-        if (is_null($this->storeInfoURL)) {
-            $this->storeInfoURL = (!empty($this->storeObject->storeInfoURL)) ? $this->storeObject->storeInfoURL : sprintf(self::ACE_LANDING_PAGE_URL_FORMAT, $this->storeNumber);
-        }
-        return $this->storeInfoURL;
+        return (!empty($this->storeObject->storeInfoURL)) ? $this->storeObject->storeInfoURL : sprintf(self::ACE_LANDING_PAGE_URL_FORMAT, $this->storeNumber);
     }
-
 
 
     /**
@@ -347,12 +191,8 @@ class Store
      */
     public function getStoreBiography()
     {
-        if (is_null($this->storeBiography)) {
-            $this->storeBiography = $this->storeObject->storeBiography;
-        }
-        return $this->storeBiography;
+        return $this->storeObject->storeBiography;
     }
-
 
 
     /**
@@ -361,16 +201,13 @@ class Store
      */
     public function getOwner()
     {
-        if (is_null($this->owner)) {
-            $owner = $this->storeObject->owner;
-            $this->owner = array_reduce($owner, function ($carry, $item) {
-                $carry .= (!empty($carry)) ? ", {$item->fullName}" : $item->fullName;
-                return $carry;
-            });
-        }
-        return $this->owner;
+        $owner = $this->storeObject->owner;
+        $owner = array_reduce($owner, function ($carry, $item) {
+            $carry .= (!empty($carry)) ? ", {$item->fullName}" : $item->fullName;
+            return $carry;
+        });
+        return $owner;
     }
-
 
 
     /**
@@ -379,19 +216,16 @@ class Store
      */
     public function getStaff()
     {
-        if (is_null($this->staff)) {
-            $staff = $this->storeObject->storeStaff;
-            $this->staff = array_map(function ($person) {
-                return [
-                    'fullName' => $person->fullName,
-                    'personTitle' => $person->personTitle,
-                    'personImageUrl' => $person->personImageUrl,
-                ];
-            }, $staff);
-        }
-        return $this->staff;
+        $staff = $this->storeObject->storeStaff;
+        $staff = array_map(function ($person) {
+            return [
+                'fullName' => $person->fullName,
+                'personTitle' => $person->personTitle,
+                'personImageUrl' => $person->personImageUrl,
+            ];
+        }, $staff);
+        return $staff;
     }
-
 
 
     /**
@@ -404,23 +238,17 @@ class Store
     }
 
 
-
     /**
      * Get the location's map coordinates
      * @return array Latitude and Longitude
      */
     public function getMapCoords()
     {
-        if (is_null($this->latitude) || is_null($this->longitude)) {
-            $this->latitude = $this->storeObject->latitude;
-            $this->longitude = $this->storeObject->longitude;
-        }
         return [
-            $this->latitude,
-            $this->longitude,
+            $this->storeObject->latitude,
+            $this->storeObject->longitude,
         ];
     }
-
 
 
     /**
@@ -429,42 +257,39 @@ class Store
      */
     public function getHours()
     {
-        if (is_null($this->hours)) {
-            $days = [
-                'Mon' => 'Monday',
-                'Tue' => 'Tuesday',
-                'Wed' => 'Wednesday',
-                'Thu' => 'Thursday',
-                'Fri' => 'Friday',
-                'Sat' => 'Saturday',
-                'Sun' => 'Sunday',
-            ];
+        $days = [
+            'Mon' => 'Monday',
+            'Tue' => 'Tuesday',
+            'Wed' => 'Wednesday',
+            'Thu' => 'Thursday',
+            'Fri' => 'Friday',
+            'Sat' => 'Saturday',
+            'Sun' => 'Sunday',
+        ];
 
-            $hours = [];
+        $hours = [];
 
-            foreach (get_object_vars($this->storeObject->hours) as $prop => $value) {
-                $hours[$prop] = str_replace('.', ':', ($value > 1200) ? sprintf("%.2fpm", ($value % 1200) / 100) : sprintf("%.2fam", ($value) / 100));
-            }
-
-            $this->hours = [
-                'Monday' => $hours["openingTimeMon"] . ' - ' . $hours['closingTimeMon'],
-                'Tuesday' => $hours["openingTimeTue"] . ' - ' . $hours['closingTimeTue'],
-                'Wednesday' => $hours["openingTimeWed"] . ' - ' . $hours['closingTimeWed'],
-                'Thursday' => $hours["openingTimeThu"] . ' - ' . $hours['closingTimeThu'],
-                'Friday' => $hours["openingTimeFri"] . ' - ' . $hours['closingTimeFri'],
-                'Saturday' => $hours["openingTimeSat"] . ' - ' . $hours['closingTimeSat'],
-                'Sunday' => $hours["openingTimeSun"] . ' - ' . $hours['closingTimeSun'],
-            ];
+        foreach (get_object_vars($this->storeObject->hours) as $prop => $value) {
+            $hours[$prop] = str_replace('.', ':', ($value > 1200) ? sprintf("%.2fpm", ($value % 1200) / 100) : sprintf("%.2fam", ($value) / 100));
         }
-        return $this->hours;
-    }
 
+        $hours = [
+            'Monday' => $hours["openingTimeMon"] . ' - ' . $hours['closingTimeMon'],
+            'Tuesday' => $hours["openingTimeTue"] . ' - ' . $hours['closingTimeTue'],
+            'Wednesday' => $hours["openingTimeWed"] . ' - ' . $hours['closingTimeWed'],
+            'Thursday' => $hours["openingTimeThu"] . ' - ' . $hours['closingTimeThu'],
+            'Friday' => $hours["openingTimeFri"] . ' - ' . $hours['closingTimeFri'],
+            'Saturday' => $hours["openingTimeSat"] . ' - ' . $hours['closingTimeSat'],
+            'Sunday' => $hours["openingTimeSun"] . ' - ' . $hours['closingTimeSun'],
+        ];
+        return $hours;
+    }
 
 
     /**
      * Return a different value for a key based on a mapping array
-     * @param  string|int   $value Original value
-     * @param  array        $map   Map of values (`'original' => 'new'`)
+     * @param  string|int $value Original value
+     * @param  array $map Map of values (`'original' => 'new'`)
      * @return string|int   The mapped value (or null if the value doesn't exist in the map)
      */
     private static function map($value, $map)
@@ -490,21 +315,18 @@ class Store
      */
     public function getDepartments()
     {
-        if (is_null($this->departments)) {
-            $departments = array_merge(
-                is_array($this->storeObject->departments) ? $this->storeObject->departments : [],
-                is_array($this->storeObject->customDepartments) ? $this->storeObject->customDepartments : []
-            );
+        $departments = array_merge(
+            is_array($this->storeObject->departments) ? $this->storeObject->departments : [],
+            is_array($this->storeObject->customDepartments) ? $this->storeObject->customDepartments : []
+        );
 
-            $map = Config::get('map', 'departments');
+        $map = Config::get('map', 'departments');
 
-            $this->departments = array_unique(array_filter(array_map(function ($item) use ($map) {
-                return self::map($item->featureLongDesc, $map);
-            }, $departments)));
-        }
-        return $this->departments;
+        $departments = array_unique(array_filter(array_map(function ($item) use ($map) {
+            return self::map($item->featureLongDesc, $map);
+        }, $departments)));
+        return $departments;
     }
-
 
 
     /**
@@ -513,21 +335,18 @@ class Store
      */
     public function getServices()
     {
-        if (is_null($this->services)) {
-            $services = array_merge(
-                is_array($this->storeObject->standardServices) ? $this->storeObject->standardServices : [],
-                is_array($this->storeObject->customServices) ? $this->storeObject->customServices : []
-            );
+        $services = array_merge(
+            is_array($this->storeObject->standardServices) ? $this->storeObject->standardServices : [],
+            is_array($this->storeObject->customServices) ? $this->storeObject->customServices : []
+        );
 
-            $map = Config::get('map', 'services');
+        $map = Config::get('map', 'services');
 
-            $this->services = array_unique(array_filter(array_map(function ($item) use ($map) {
-                return self::map($item->featureLongDesc, $map);
-            }, $services)));
-        }
-        return $this->services;
+        $services = array_unique(array_filter(array_map(function ($item) use ($map) {
+            return self::map($item->featureLongDesc, $map);
+        }, $services)));
+        return $services;
     }
-
 
 
     /**
@@ -536,21 +355,18 @@ class Store
      */
     public function getBrands()
     {
-        if (is_null($this->brands)) {
-            $brands = array_merge(
-                is_array($this->storeObject->specialtyBrands) ? $this->storeObject->specialtyBrands : [],
-                is_array($this->storeObject->customSpecialtyBrand) ? $this->storeObject->customSpecialtyBrand : []
-            );
+        $brands = array_merge(
+            is_array($this->storeObject->specialtyBrands) ? $this->storeObject->specialtyBrands : [],
+            is_array($this->storeObject->customSpecialtyBrand) ? $this->storeObject->customSpecialtyBrand : []
+        );
 
-            $map = Config::get('map', 'brands');
+        $map = Config::get('map', 'brands');
 
-            $this->brands = array_unique(array_filter(array_map(function ($item) use ($map) {
-                return self::map($item->featureLongDesc, $map);
-            }, $brands)));
-        }
-        return $this->brands;
+        $brands = array_unique(array_filter(array_map(function ($item) use ($map) {
+            return self::map($item->featureLongDesc, $map);
+        }, $brands)));
+        return $brands;
     }
-
 
 
     /**
@@ -559,15 +375,27 @@ class Store
      */
     public function getChain()
     {
-        if (is_null($this->chain)) {
-            $chain = $this->storeObject->storeChain;
-            $this->chain = array_map(function ($store) {
-                return [
-                    'locationName' => $store->locationName,
-                    'locationCode' => intval($store->locationCode),
-                ];
-            }, $chain);
+        $chain = $this->storeObject->storeChain;
+        $chain = array_map(function ($store) {
+            return [
+                'locationName' => $store->locationName,
+                'locationCode' => intval($store->locationCode),
+            ];
+        }, $chain);
+        return $chain;
+    }
+
+    /**
+     * Format a phone number in the US-style
+     *
+     * @param $number
+     * @return string
+     */
+    private function phoneNumberfy($number)
+    {
+        if (preg_match('/^(\d{3})(\d{3})(\d{4})$/', (string)$number, $matches)) {
+            return sprintf('(%s) %s-%s', $matches[1], $matches[2], $matches[3]);
         }
-        return $this->chain;
+        return (string)$number;
     }
 }
